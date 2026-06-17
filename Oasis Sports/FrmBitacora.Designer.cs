a82ec46b0bf2aa79830e6fdeda5f8f6a7960b246
Lambezat, Juan Ignacio <juanlambezat@gmail.com>
2026-06-17 00:42:38 -0300

    primer filtro bitacora

--- Oasis Sports/FrmBitacora.Designer.cs
+++ Oasis Sports/FrmBitacora.Designer.cs
@@ -29,6 +29,17 @@
         private void InitializeComponent()
         {
             dgvBitacora = new DataGridView();
+            txtUsuario = new TextBox();
+            cmbCriticidad = new ComboBox();
+            dtpDesde = new DateTimePicker();
+            dtpHasta = new DateTimePicker();
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
             ((System.ComponentModel.ISupportInitialize)dgvBitacora).BeginInit();
             SuspendLayout();
             // 
@@ -37,29 +48,146 @@
             dgvBitacora.AllowUserToAddRows = false;
             dgvBitacora.BackgroundColor = Color.DarkGreen;
             dgvBitacora.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvBitacora.Dock = DockStyle.Fill;
-            dgvBitacora.Location = new Point(0, 0);
+            dgvBitacora.Location = new Point(316, 12);
             dgvBitacora.Name = "dgvBitacora";
             dgvBitacora.ReadOnly = true;
-            dgvBitacora.Size = new Size(658, 450);
+            dgvBitacora.Size = new Size(708, 504);
             dgvBitacora.TabIndex = 0;
             dgvBitacora.CellContentClick += dgvBitacora_CellContentClick;
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(76, 81);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(121, 23);
+            txtUsuario.TabIndex = 1;
+            // 
+            // cmbCriticidad
+            // 
+            cmbCriticidad.FormattingEnabled = true;
+            cmbCriticidad.Location = new Point(76, 111);
+            cmbCriticidad.Name = "cmbCriticidad";
+            cmbCriticidad.Size = new Size(121, 23);
+            cmbCriticidad.TabIndex = 2;
+            // 
+            // dtpDesde
+            // 
+            dtpDesde.Location = new Point(76, 145);
+            dtpDesde.Name = "dtpDesde";
+            dtpDesde.Size = new Size(121, 23);
+            dtpDesde.TabIndex = 3;
+            // 
+            // dtpHasta
+            // 
+            dtpHasta.Location = new Point(76, 183);
+            dtpHasta.Name = "dtpHasta";
+            dtpHasta.Size = new Size(121, 23);
+            dtpHasta.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(20, 227);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 5;
+            button1.Text = "Buscar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(122, 227);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 6;
+            button2.Text = "Limpiar";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(53, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(110, 15);
+            label1.TabIndex = 7;
+            label1.Text = "Bitacora de eventos";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 84);
+            label2.Name = "label2";
+            label2.Size = new Size(47, 15);
+            label2.TabIndex = 8;
+            label2.Text = "Usuario";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 145);
+            label3.Name = "label3";
+            label3.Size = new Size(39, 15);
+            label3.TabIndex = 9;
+            label3.Text = "Desde";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(14, 183);
+            label4.Name = "label4";
+            label4.Size = new Size(37, 15);
+            label4.TabIndex = 10;
+            label4.Text = "Hasta";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 114);
+            label5.Name = "label5";
+            label5.Size = new Size(58, 15);
+            label5.TabIndex = 11;
+            label5.Text = "Criticidad";
+            label5.Click += label5_Click;
             // 
             // FrmBitacora
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(658, 450);
+            ClientSize = new Size(1036, 631);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(dtpHasta);
+            Controls.Add(dtpDesde);
+            Controls.Add(cmbCriticidad);
+            Controls.Add(txtUsuario);
             Controls.Add(dgvBitacora);
             Name = "FrmBitacora";
             Text = "FrmBitacora";
             Load += FrmBitacora_Load_1;
             ((System.ComponentModel.ISupportInitialize)dgvBitacora).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private DataGridView dgvBitacora;
+        private TextBox txtUsuario;
+        private ComboBox cmbCriticidad;
+        private DateTimePicker dtpDesde;
+        private DateTimePicker dtpHasta;
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
     }
 }
--- Oasis Sports/FrmBitacora.cs
+++ Oasis Sports/FrmBitacora.cs
@@ -15,6 +15,8 @@ namespace Oasis_Sports
     {
         BLL_Evento bllEvento = new BLL_Evento();
 
+        DataTable tablaEventos;
+
         public FrmBitacora()
         {
             InitializeComponent();
@@ -22,7 +24,9 @@ namespace Oasis_Sports
 
         private void CargarBitacora()
         {
-            dgvBitacora.DataSource = bllEvento.ListarEventos();
+            tablaEventos = bllEvento.ListarEventos();
+
+            dgvBitacora.DataSource = tablaEventos;
         }
 
         private void dgvBitacora_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -31,8 +35,65 @@ namespace Oasis_Sports
         }
 
         private void FrmBitacora_Load_1(object sender, EventArgs e)
-        {           
+        {
             CargarBitacora();
+
+            cmbCriticidad.Items.Add("Todas");
+            cmbCriticidad.Items.Add("1");
+            cmbCriticidad.Items.Add("2");
+            cmbCriticidad.Items.Add("3");          
+            cmbCriticidad.SelectedIndex = 0;
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataView vista = tablaEventos.DefaultView;
+
+            string filtro = "";
+
+            // Usuario
+            if (!string.IsNullOrWhiteSpace(txtUsuario.Text))
+            {
+                filtro += $"Usuario LIKE '%{txtUsuario.Text}%'";
+            }
+
+            // Criticidad
+            if (cmbCriticidad.Text != "Todas")
+            {
+                if (filtro != "")
+                    filtro += " AND ";
+
+                filtro += $"Criticidad = {cmbCriticidad.Text}";
+            }
+
+            // Fechas
+            if (filtro != "")
+                filtro += " AND ";
+
+            filtro += $"Fecha >= #{dtpDesde.Value:yyyy-MM-dd}#";
+            filtro += $" AND Fecha <= #{dtpHasta.Value:yyyy-MM-dd 23:59:59}#";
+
+            vista.RowFilter = filtro;
+
+            dgvBitacora.DataSource = vista;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtUsuario.Clear();
+
+            cmbCriticidad.SelectedIndex = 0;
+
+            dtpDesde.Value = DateTime.Today.AddMonths(-1);
+
+            dtpHasta.Value = DateTime.Today;
+
+            dgvBitacora.DataSource = tablaEventos;
         }
     }
 }
